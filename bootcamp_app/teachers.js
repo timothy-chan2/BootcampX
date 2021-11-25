5f7670ac745f6074b0a83d53a9bd84915cf6d449
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-25 04:27:09 +0000

    Use node postgres to turn query result into JavaScript object & print result

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,15 +13,18 @@ let inputs = process.argv.slice(2);
 
 // Using a promise to query
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${inputs[0]}%'
-LIMIT ${inputs[1] || 5};
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(*) AS total_assistances
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = '${inputs[0] || 'JUL02'}'
+  GROUP BY teacher, cohort
+  ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
